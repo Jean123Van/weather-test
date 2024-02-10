385e989b514f7d572ed5049dcae96d664ab695f6
cklph-jean <jean@chykalophia.com>
2024-02-10 20:43:07 +0800

    layout of landig page

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 function Landing(props) {
-  return <div>landing page</div>;
+  const description =
+    "Welcome to the weather forecast web application. Please login with your Github user to use the application and view the weather in your city.";
+
+  return (
+    <div>
+      <p>{description}</p>
+      <button>Login</button>
+    </div>
+  );
 }
 
 export default Landing;
