17369883e71a3dd4a9ea85b276c9606f7167ca3e
cklph-jean <jean@chykalophia.com>
2024-02-10 20:09:03 +0800

    render navbar component

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,17 @@
 import React from "react";
+import "./Navbar.css";
+import { FaCloudSunRain } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 function Navbar(props) {
-  return <div>navbar</div>;
+  return (
+    <div className="navbar-cntr">
+      <Link to="/">
+        <FaCloudSunRain className="navbar-logo" />
+      </Link>
+      <h1>Weather Forecast</h1>
+    </div>
+  );
 }
 
 export default Navbar;
