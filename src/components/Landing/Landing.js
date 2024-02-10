646ddcf0f00924907fb21c5e1f5116d6e13ed413
cklph-jean <jean@chykalophia.com>
2024-02-10 21:29:08 +0800

    authO integration with github login and render landing page

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,13 +1,19 @@
 import React from "react";
+import "./Landing.css";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function Landing(props) {
   const description =
     "Welcome to the weather forecast web application. Please login with your Github user to use the application and view the weather in your city.";
 
+  const { loginWithPopup } = useAuth0();
+
   return (
-    <div>
+    <div className="landing-cntr">
       <p>{description}</p>
-      <button>Login</button>
+      <button onClick={() => loginWithPopup({ connection: "github" })}>
+        Login
+      </button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="dev-82iwuozckwsnqhwr.us.auth0.com"
+      clientId="kHPIdRLtwc9Or3Bibcy0th5sHJq8veAB"
+      redirectUri={window.location.origin}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
 
