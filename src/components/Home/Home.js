02bd1138bc4ec289ba082651dba398c46d7bc1e9
cklph-jean <jean@chykalophia.com>
2024-02-11 01:31:24 +0800

    add functionality to display weather button

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,20 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaSearch } from "react-icons/fa";
 import "./Home.css";
 import "../../styles/button.css";
+import axios from "axios";
 
 function Home(props) {
   const { name, nickname } = props.user;
+  const [city, setCity] = useState("");
+
+  async function showWeather() {
+    // const value = await axios({
+    //   method: "GET",
+    //   url: "http://api.openweathermap.org/geo/1.0/direct?q=london&appid=ccfab6ed8ffcb012dedbd4447d7f81bf",
+    // }).catch(() => {
+    //   throw new Error();
+    // });
+
+    console.log(city);
+
+    // window.location.href = "/weather?long=1&lat=2";
+  }
 
   return (
     <div className="home-cntr">
       <h2>{name || nickname}</h2>
       <h3>{`https://github.com/${nickname}`}</h3>
       <div className="home-search">
-        <input type="text" placeholder="City" />
+        <input
+          type="text"
+          placeholder="City"
+          value={city}
+          onChange={(e) => {
+            setCity(e.target.value);
+          }}
+        />
         <FaSearch className="home-search-icon" />
       </div>
-      <button className="button-primary">Display Weather</button>
+      <button
+        className="button-primary"
+        onClick={() => {
+          showWeather();
+        }}
+      >
+        Display Weather
+      </button>
     </div>
   );
 }
--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -1,6 +1,16 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 function Weather(props) {
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+
+  const long = queryParams.get("long");
+  const lat = queryParams.get("lat");
+
+  console.log(long);
+  console.log(lat);
+
   return <div>weather</div>;
 }
 
