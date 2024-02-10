8b9219280bfc0c8d7e3660303163e74dc8027444
cklph-jean <jean@chykalophia.com>
2024-02-10 22:02:54 +0800

    addition of logout button to navbar

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -6,13 +6,23 @@ function Landing(props) {
   const description =
     "Welcome to the weather forecast web application. Please login with your Github user to use the application and view the weather in your city.";
 
-  const { loginWithPopup } = useAuth0();
+  const { loginWithPopup, logout, isAuthenticated, user, isLoading } =
+    useAuth0();
+
+  // console.log(isAuthenticated, isLoading);
 
   return (
     <div className="landing-cntr">
       <p>{description}</p>
       <button onClick={() => loginWithPopup({ connection: "github" })}>
         Login
+      </button>
+      <button
+        onClick={() =>
+          logout({ logoutParams: { returnTo: window.location.origin } })
+        }
+      >
+        Logout
       </button>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -6,15 +6,15 @@ import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Auth0Provider
-      domain="dev-82iwuozckwsnqhwr.us.auth0.com"
-      clientId="kHPIdRLtwc9Or3Bibcy0th5sHJq8veAB"
-      redirectUri={window.location.origin}
-    >
-      <App />
-    </Auth0Provider>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <Auth0Provider
+    domain="dev-82iwuozckwsnqhwr.us.auth0.com"
+    clientId="kHPIdRLtwc9Or3Bibcy0th5sHJq8veAB"
+    redirectUri={window.location.origin}
+  >
+    <App />
+  </Auth0Provider>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
